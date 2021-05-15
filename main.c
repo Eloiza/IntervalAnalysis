351e53142e77b9e6e378d899d5ff35fcde480575
Eloiza <eloiza.rossetto@gmail.com>
2021-05-14 22:03:49 -0300

    Implemented a function to read a float

--- main.c
+++ main.c
@@ -4,10 +4,45 @@
 
 #define BUFFER_SIZE 100
 
-void read_stdin(){
-    char line_buffer[BUFFER_SIZE];
+#define ADD 1
+#define SUB 2
+#define MUL 3
+#define DIV 4
+
+
+// int * read_operation(){
+// [operation, index1, index2]
+// };
+
+float read_float(){
+    float number;
+    int count;
     char * token;
-    int count = 0;
+    char buffer[BUFFER_SIZE];
+
+    if(fgets(buffer, BUFFER_SIZE, stdin) == NULL){
+        printf("Error to catch a line\n");
+
+    }else{
+        //split the string
+        token = strtok(buffer, " ");
+        count = 0;
+        while(token != NULL){
+            if(count){
+                //remove the \n and get a float number
+                token = strtok(token, "\n");
+                number = atof(token);
+            }
+            count++;
+
+            token = strtok(NULL, " ");
+        }
+    }
+
+    return number;
+}
+void read_stdin(float ** numbers, float ** operations){
+    char line_buffer[BUFFER_SIZE];
 
     //read the first line to know how many numbers and operations we will read
     int size_numbers, size_operations;
@@ -20,35 +55,27 @@ void read_stdin(){
     //a wild getchat to give us time
     getchar();
 
+    *numbers = malloc(sizeof(float) * size_numbers);
+    *operations = malloc(sizeof(float) * size_operations);
+
     //read all the numbers
     for(int i=0; i<size_numbers; i++){
-        if(fgets(line_buffer, BUFFER_SIZE, stdin) == NULL){
-            printf("Error to catch a line\n");
-
-        }else{
-            printf("I read: %s", line_buffer);
-
-            //divide the string
-            token = strtok(line_buffer, " ");
-            count = 0;
-            while(token != NULL){
-                if(count){
-                    //remove the \n and get a float number
-                    token = strtok(token, "\n");
-                    printf("str num: %s\n", token);
-                    printf("number: %f\n",atof(token));
-                }
-                count++;
-                // printf("token: %s\n", token);
-                token = strtok(NULL, " ");
-            }
-        }
+        (*numbers)[i] = read_float();
+        printf("%f\n", *numbers[i]);
     }
+
+    // for(int i=0; i<size_operations; i++){
+    //     (*operations)[i] = read_operation();
+    // }
 }
 
 int main(){
-    // int * numbers, operations;
+    float * numbers, * operations;
 
-    read_stdin();
+    read_stdin(&numbers, &operations);
+
+    free(numbers);
+    free(operations);
+
     return 0;
 }
