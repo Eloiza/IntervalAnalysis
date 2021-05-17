d7f79ce5b49f484edabd84beba9052195fa2bb34
Eloiza <eloiza.rossetto@gmail.com>
2021-05-17 17:50:40 -0300

    Add a new lib file calculation.h to handle all calculations needed

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "read_stdin.h"
+#include "calculation.h"
 
 int main(){
     float * numbers;    //[1.54, 2.2, 3.21]
-    int ** operations;  //[[0,2,1]] num[op[0][0]]
+    int ** operations;  //[[0,2,1,1]] num[op[0][0]]
     int size_numbers, size_operations;
 
     read_stdin(&numbers, &operations, &size_numbers, &size_operations);
